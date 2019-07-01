5128b87e93ae337ca016ea696011d9ae352a0061
Jeremy Soller <jeremy@system76.com>
2019-07-01 13:32:22 -0600

    0.1.4 - add support for using std

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-#![no_std]
-#![feature(alloc)]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), feature(alloc))]
 
+#[cfg(not(feature = "std"))]
 #[macro_use]
 extern crate alloc;
-extern crate plain;
 
-use alloc::string::String;
-use alloc::vec::Vec;
+#[cfg(not(feature = "std"))]
+use alloc::{string::String, vec::Vec};
 use plain::Plain;
 
 #[repr(packed)]
@@ -142,36 +142,6 @@ pub struct ChassisInfo {
 }
 
 unsafe impl Plain for ChassisInfo {}
-
-#[repr(packed)]
-#[derive(Default, Debug)]
-pub struct ProcessorInfo {
-    pub socket_designation: u8,
-    pub processor_kind: u8,
-    pub processor_family: u8,
-    pub processor_manufacturer: u8,
-    pub processor_id: u64,
-    pub processor_version: u8,
-    pub voltage: u8,
-    pub external_clock: u16,
-    pub max_speed: u16,
-    pub current_speed: u16,
-    pub status: u8,
-    pub processor_upgrade: u8,
-    pub l1_cache_handle: u16,
-    pub l2_cache_handle: u16,
-    pub l3_cache_handle: u16,
-    pub serial_number: u8,
-    pub asset_tag: u8,
-    pub part_number: u8,
-    pub core_count: u8,
-    pub core_enabled: u8,
-    pub thread_count: u8,
-    pub processor_characteristics: u16,
-    pub processor_family_2: u16,
-}
-
-unsafe impl Plain for ProcessorInfo {}
 
 #[repr(packed)]
 #[derive(Default, Debug)]
